74865b418ad1c67e7fb916de0a548efdf65e3f24
KirkSong <kirksong0420@gmail.com>
2021-08-11 11:57:38 +0800

    v1.1.3 Chapter1 Section3_4

--- C++ Primer/Chapter 1/Example/Example/Example.cpp
+++ C++ Primer/Chapter 1/Example/Example/Example.cpp
@@ -5,11 +5,87 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
-	std::cout << "Please Enter Tow Numbers:" << std::endl;
-	int v1 = 0, v2 = 0;
-	std::cin >> v1 >> v2;
-	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+    std::cout << "hello world!\n";
+
+	//std::cout << "please enter tow numbers:" << std::endl;
+	//int v1 = 0, v2 = 0;
+	//std::cin >> v1 >> v2;
+	//std::cout << "the sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+
+	//int sum = 0, value = 50;
+	//while (value <= 100)
+	//{
+	//	sum += value;
+	//	++value;
+	//
+	//}
+	//std::cout << "Sum of 50 to 100 inclusive is " << sum << std::endl;
+
+	//int value = 10;
+	//while (value >= 0)
+	//{
+	//	std::cout << value << std::endl;
+	//	--value;
+	//}
+
+	//int value_in1 = 0, value_in2 = 0;
+	//int value_min = 0, value_max = 0;
+
+	//std::cout << "Please input two nums: " << std::endl;
+	//std::cin >> value_in1 >> value_in2;
+
+	//if (value_in1 <= value_in2)
+	//{
+	//	value_min = value_in1;
+	//	value_max = value_in2;
+	//}
+	//else
+	//{
+	//	value_min = value_in2;
+	//	value_max = value_in1;
+	//}
+
+	//while (value_max >= value_min)
+	//{
+	//	std::cout << value_max << std::endl;
+	//	--value_max;
+	//}
+
+	//std::cout << value_in1 << " " << value_in2 << " " << value_min << " " << value_max << " "  << std::endl;
+
+	//int sum = 0, value = 0;
+	//while (std::cin >> value)
+	//{
+	//	sum += value;
+	//}
+	//std::cout << "sum is: " << sum << std::endl;
+
+	// 问题就是 如果后续再来一个跟前边相同的数字，没法将出现次数再次加到前边去。
+	int curValue = 0, value = 0;
+	if (std::cin >> curValue)
+	{
+		int count_curValue = 1;
+		while (std::cin >> value)
+		{
+			if (curValue == value)
+			{
+				count_curValue++;
+			}
+			else  //说明 接下来的这个数已经不是原来的数了
+			{
+				std::cout << "The number " << curValue << " occurs " << count_curValue << " times " << std::endl;
+
+				curValue = value;
+				count_curValue = 1;
+			}
+
+		}
+		std::cout << "The number " << curValue << " occurs " << count_curValue << " times " << std::endl;
+	}
+
+	return 0;
+
+
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
